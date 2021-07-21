871c56ecf0528ebb0ae9cc42f942442292da7b9a
Gordon Byers <gobyers@microsoft.com>
2021-07-21 22:51:50 +0100

    Project init

---
+++ CheckForSubscription/run.ps1
@@ -0,0 +1,9 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger initiated."
+
+Write-Output $Request.Body
